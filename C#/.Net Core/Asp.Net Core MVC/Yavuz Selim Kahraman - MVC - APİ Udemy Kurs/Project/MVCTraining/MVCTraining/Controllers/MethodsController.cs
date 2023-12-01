111771513616a8ff725b891e87917a0270c627c6
Berkay Akar <mberkayakar@outlook.com>
2023-12-01 23:47:13 +0300

    dosya yolları değişti ve Son bir MVC tekrarı yapıldı

---
+++ C#/.Net Core/Asp.Net Core MVC/Yavuz Selim Kahraman - MVC - APİ Udemy Kurs/Project/MVCTraining/MVCTraining/Controllers/MethodsController.cs
@@ -0,0 +1,37 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public class MethodsController : Controller
+    {
+        // View methodu : içerisinde bulunduğu Action methodu ile (action un ismmi) aynı isimdeki view i gelir bana gösterir. 
+        // yani methodun ismindeki view i ilgili controller yada shared içerisidne arar ve method ismindeki viewi  getirir 
+        public IActionResult Index()
+        { 
+            // Index Viewini getirecek 
+            return View();
+        }
+
+        public IActionResult Ahmet()
+        {
+            // Ahmet Viewini getirecek 
+            return View();
+        }
+
+        public IActionResult Mehmet()
+        {
+            // Mehmet Viewini getirecek 
+            return View();
+        }
+        public IActionResult Hasan()
+        {
+            // Evet method ismi Hasan ama view içerisinde isim belirtilmiş Huseyin ismindeki viewi bulup getirecek
+            return View("Huseyin");
+        }
+
+
+
+
+
+    }
+}
---
+++ C#/.Net Core/Asp.Net Core MVC/Yavuz Selim Kahraman - MVC - APİ Udemy Kurs/Project/MVCTraining/MVCTraining/Controllers/RazorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public class RazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
